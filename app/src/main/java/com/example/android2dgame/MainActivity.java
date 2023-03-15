bd4a2235b8f13efabdb2b2ab441d30a5164fac26
Austen Leow <austenleow.al@gmail.com>
2023-03-15 18:59:34 +0800

    rendering game on mainactivity and implementing game loop (threading)

--- app/src/main/java/com/example/android2dgame/MainActivity.java
+++ app/src/main/java/com/example/android2dgame/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.android2dgame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        // set window to fullscreen (hide status bar)
+        Window window = getWindow();
+        window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
+
+        // set content view to game, so that objects in the Game class can be rendered to the screen
+        setContentView(new Game(this));
     }
+
 }
